0ae0d51c02b6fe1f168c47c9eab710e91a8cd3aa
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-03-20 15:42:00 -0700

    added habit tracker table creator with js

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
+// HABIT TRACKER
+const monthHeader = document.querySelector(".month-header");
+const habitTable = document.querySelector(".habit-table");
+const habitDate = document.querySelector(".habit-date");
+const habit = document.querySelectorAll(".habit");
+const habitAmount = habit.length;
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const date = new Date();
+let month = date.getMonth();
+monthHeader.innerHTML = months[month];
+
+function setHabitTable(month) {
+  let numDays;
+  //sets up # of dates per month
+  if (month < 5 && month % 2 == 0) {
+    numDays = 31;
+  } else {
+    numDays = 30;
+  }
+
+  for (let i = 0; i < numDays; i++) {
+    //Header Dates
+    const th = document.createElement("th");
+    th.innerHTML = i + 1;
+    habitDate.appendChild(th);
+
+    //Habit cells
+    for (let i = 0; i < habitAmount; i++) {
+      const td = document.createElement("td");
+      td.innerHTML = "x";
+      habit[i].insertCell();
+    }
+  }
+}
+
+setHabitTable(month);
+
+// TODO-LIST
+
 let todoContainer = document.querySelector(".todo-container");
 const todoList = document.querySelector(".todo-list");
 const inputField = document.querySelector(".todo-item");
