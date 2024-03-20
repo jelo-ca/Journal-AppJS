bd0519869e758abfda36c895cba96115ccc97412
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-03-20 09:17:58 -0700

    new todolist using localstorage and ul

--- main.js
+++ main.js
@@ -1,64 +1,41 @@
-let todoList = document.querySelector(".todo-container");
-const todoTemplate = document.querySelector(".todo-template");
-const todoAddButton = document.querySelector(".add-todo");
-const todoDeleteButton = document.querySelector(".todo-delete");
-
-let todos = getTodos();
-
-function getTodos() {
-  const val = localStorage.getItem("todo") || "[]";
-
-  return JSON.parse(val);
-}
-
-function setTodos(todos) {
-  const todoJson = JSON.stringify(todos);
-
-  localStorage.setItem("todo", todoJson);
-}
-
-function updateTodo(todo, key, val) {
-  todo[key] = val;
-
-  setTodos(todos);
-  refreshList;
-}
+let todoContainer = document.querySelector(".todo-container");
+const todoList = document.querySelector(".todo-list");
+const inputField = document.querySelector(".todo-item");
 
 function addTodo() {
-  todos.unshift({
-    description: "",
-    completed: false,
-  });
-
-  setTodos(todos);
-  refreshList();
-}
-
-function deleteTodo(key) {
-  todos.splice(key, 1);
-  setTodos(todos);
-  refreshList();
-}
-
-function refreshList() {
-  todoList.innerHTML = "";
-
-  for (const todo of todos) {
-    let itemElement = todoTemplate.content.cloneNode(true);
-    const descriptionInput = itemElement.querySelector(".todo-item");
-    const checkInput = itemElement.querySelector(".todo-complete");
-
-    descriptionInput.value = todo.description;
-    checkInput.checked = todo.completed;
-
-    todoList.append(itemElement);
+  if (inputField.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    todoList.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputField.value = "";
+  saveData();
 }
 
-// JUST TO CLEAR WHILE MAKING deleteTodo()
-localStorage.clear();
+todoList.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+    saveData();
+  },
+  false
+);
 
-todoAddButton.add;
-todoAddButton.addEventListener("click", () => addTodo());
-todoDeleteButton.addEventListener("click", () => deleteTodo(1));
-refreshList();
+function saveData() {
+  localStorage.setItem("todo-data", todoList.innerHTML);
+}
+
+function showTasks() {
+  todoList.innerHTML = localStorage.getItem("todo-data");
+}
+
+showTasks();
